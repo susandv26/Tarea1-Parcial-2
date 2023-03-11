1a568144d92b0bd45ac6727278d66d4cb86070a1
susandv26 <valladares.susan@yahoo.com>
2023-03-11 01:14:39 -0600

    creacion de proyecto Django implementando GET

---
+++ tutorials/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http.response import JsonResponse
+from rest_framework.parsers import JSONParser 
+from rest_framework import status
+ 
+from tutorials.models import Tutorial
+from tutorials.serializers import TutorialSerializer
+from rest_framework.decorators import api_view
+  
+@api_view(['GET'])
+def tutorial_list_published(request):
+    tutorials = Tutorial.objects.filter(published=True)
+        
+    if request.method == 'GET': 
+        tutorials_serializer = TutorialSerializer(tutorials, many=True)
+        return JsonResponse(tutorials_serializer.data, safe=False)
